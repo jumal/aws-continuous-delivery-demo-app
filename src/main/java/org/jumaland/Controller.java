e9e7ef37be5850fa20e77225b92d84248087acb8
jumal <jumal@apache.org>
2016-02-27 19:31:08 +0000

    Initial commit

---
+++ src/main/java/org/jumaland/Controller.java
@@ -0,0 +1,17 @@
+package org.jumaland;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() throws UnknownHostException {
+        InetAddress localhost = InetAddress.getLocalHost();
+        return "Hello from " + localhost.getHostName() + " (" + localhost.getHostAddress() + ")!";
+    }
+}
